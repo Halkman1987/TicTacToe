6a4dac9d54589ee5334fcc21c48de0cc0228d7ab
halkman <halk-man@mail.ru>
2024-12-19 15:46:30 +0500

    проба

--- MainForm.cs
+++ MainForm.cs
@@ -79,6 +79,7 @@ namespace TicTacToe
         {
             if (TargetSymbol == "X")
             {
+                
                 //рисование крестика
             }
             else if (TargetSymbol == "O")
